c84ed75d0dd5ce7d511fdfdf4f85c5147202a51c
nicolpena <nicol.daniela3110@gmail.com>
2022-08-30 16:05:57 -0500

    clase29-08-2022 8-pm

---
+++ jsLogin.js
@@ -0,0 +1,11 @@
+const user = "nicolpena"
+const pw = 1234
+function verificar(){
+    let usuario = document.getElementById("nombre").value
+    let clave = document.getElementById("clave").value
+    if(usuario == user && clave == pw){
+        alert("Login existoso")
+    }else{
+        alert("Verifique los datos ingresados")
+    }
+}
